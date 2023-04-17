75c7db04fe70623820ba6b44f0f262209e409ab6
devaliy <indexcorpng@gmail.com>
2023-04-17 12:25:00 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'redbricks',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
